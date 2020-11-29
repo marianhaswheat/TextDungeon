69ea5b7f7c65095d0cc83dca783e24da67a56167
Marian <marianhaswheat@gmail.com>
2020-11-29 13:42:06 -0700

    TitleScreen

--- TextDungeon/TextDungeon.cpp
+++ TextDungeon/TextDungeon.cpp
@@ -1,9 +1,47 @@
+//
 // TextDungeon.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <string>
 
+void printf(std::string str)
+{
+    std::cout << str;
+    std::cout << std::endl;
+}
+
+void StartScreen()
+{
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \0");
+    printf(": Hello! Welcome to Text Dungeon. A text based game that runs on CPP. :");
+    printf(": Created by Marian and Sphinx.                                       :");
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    printf(": 1)                            start                                 :");
+    printf(": 2)                            exit                                  :");
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+    unsigned int input;
+    std::cin >> input;
+
+    if (input == 1)
+    {
+        while (true)
+        {
+
+        }
+    }
+    else if (input == 2)
+    {
+        return;
+    }
+    else
+    {
+        StartScreen();
+    }
+
+}
 int main()
 {
-    std::cout << "Hello Universe!\n";
+    StartScreen();
 }
